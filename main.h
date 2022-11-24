681d4f7706da223944a813cf359c98047d21faf3
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-24 21:28:36 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,49 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
 
 int _putchar(char c);
-int _printf(const char *format, ...);
-int _print_char(va_list args);
-int _print_int(va_list args);
-int _print_str(va_list args);
-int (*_selct_func(char c))(va_list);
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int _print_char(va_list);
+int _print_int(va_list);
+int _print_str(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
+int buffer(char);
+
+/**
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
 
 
 #endif
