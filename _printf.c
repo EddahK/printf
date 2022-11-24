da3a868979d361b43336966745dbb2f369cb7b0d
EddahK <wangu.kin@gmail.com>
2022-11-24 03:16:54 -0800

    task 0

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stddef.h>
+
 /**
   * _printf - function that prints std input
   * @format: format of input - c, %, s
@@ -11,8 +12,8 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, k = 0;
+	int n_displayed = 0;
 	char *str = NULL;
-	
 
 	va_start(args, format);
 
@@ -20,11 +21,13 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 			_putchar(format[i]);
+			n_displayed++;
 		else
 		{
 			if (format[i + 1] == 'c')
 			{
 				_putchar(va_arg(args, int));
+				n_displayed++;
 				i++;
 			}
 			else if (format[i + 1] == 's')
@@ -35,14 +38,21 @@ int _printf(const char *format, ...)
 				while (str[k] != '\0')
 				{
 					_putchar(str[k]);
+					n_displayed++;
 					k++;
 				}
 
-			}	
+
+			}
+			else if (format[i + 1] == '%')
+			{
+				i++;
+				_putchar('%');
+				n_displayed++;
+			}
 		}
 		i++;
 	}
-
 	va_end(args);
-	return (0);
+	return (n_displayed);
 }
--- test/main.c
+++ test/main.c
@@ -1,12 +1,26 @@
 #include "../main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/* gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format main.c ../*.c */
 
 int main(void)
 {
 	/* %% %c % s*/
+	int n = 0;
 
-	
-	_printf("A+ %c", "1");
+	n = _printf("%%\n");
 
-	return (0);
+	printf("n: %d\n", n);
+
+	 n = printf("%%\n");
+	 printf("n: %d\n", n);
+
+	_printf("%%\n");
+	_printf("Name: %s\n", "Betty");
+	_printf("Grade: %c\n", "1");
+
+	printf("n: %d\n", n);
+
+	return (n);
 }
