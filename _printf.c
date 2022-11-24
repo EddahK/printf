c163fe4f66d8520a07b60e05fe1b037735e62034
EddahK <47106054+EddahK@users.noreply.github.com>
2022-11-24 21:19:17 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,31 +10,21 @@
   */
 int _printf(const char *format, ...)
 {
+	int size;
 	va_list args;
-	int i = 0, k = 0, num = 0;
-	int n_displayed = 0;
-	char *str = NULL;
-        int (*func)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	size = _strlen(format);
+	if (size <= 0)
+		return (0);
 
 	va_start(args, format);
+	size = handler(format, args);
 
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-                {
-                        func = _select_func(format[i + 1])
-                        if (func != NULL)
-			{
-				
-				n_displayed += func(args);
-				i++;
-
-                }
-			
-				
-				
-		i++;
-	}
+	_putchar(-1);
 	va_end(args);
-	return (n_displayed);
+
+	return (size);
 }
